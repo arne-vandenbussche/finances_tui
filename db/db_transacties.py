07d764d6cd97eab6e4c57d46d3784f2b527e9f32
arne-vandenbussche <arne.vandenbussche@gmail.com>
2025-04-15 19:48:05 +0200

    CRUD in database implemented

--- db/db_transacties.py
+++ db/db_transacties.py
@@ -45,7 +45,7 @@ def convert_transaction_row_to_object(transaction_row: tuple) -> Transactie:
     tr_categorie = transaction_row[9]
     tr_actuele_rekeningstand = transaction_row[10]
     return Transactie(id=tr_id, datum=tr_datum, bedrag=tr_bedrag, betaalwijze=tr_betaalwijze,
-                      rekeningnummer= tr_rekeningnummer, van_aan=tr_van_aan, 
+                      rekeningnummer= tr_rekeningnummer, van_aan=tr_van_aan,
                       beschrijving=tr_beschrijving, factuurnummer=tr_factuurnummer,
                       factuurdatum=tr_factuurdatum, categorie=tr_categorie,
                       actuele_rekeningstand=tr_actuele_rekeningstand)
@@ -57,7 +57,7 @@ def get_connection() -> sqlite3.Connection:
     db_directory = Path.cwd()
     full_db_path = db_directory / db_file_name
     is_new_db = not full_db_path.exists()
-    
+
     # Connect to database. The database will be created if it does not exists
     conn = sqlite3.connect(full_db_path)
     logger.info(f"Connecting to database on {full_db_path}")
@@ -67,7 +67,7 @@ def get_connection() -> sqlite3.Connection:
         initialize_database(conn)
 
     return conn
-        
+
 
 def initialize_database(conn: sqlite3.Connection) -> None:
     cursor = conn.cursor()
@@ -99,7 +99,60 @@ def get_all_transactions() -> list[Transactie]:
     return transaction_list
 
 
+def get_transaction_by_id(transaction_id: int) -> Transactie:
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM transacties WHERE id = ?", (transaction_id,))
+    transaction_row = cursor.fetchone()
+    conn.close()
+    if transaction_row:
+        return convert_transaction_row_to_object(transaction_row)
+    else:
+        return None
 
 
+def get_transactions_by_date(transaction_date: date) -> list[Transactie]:
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM transacties WHERE datum = ?", (transaction_date,))
+    rows = cursor.fetchall()
+    conn.close()
+    transaction_list = [convert_transaction_row_to_object(row) for row in rows]
+    return transaction_list
+    def get_transaction_by_description(part_description: str) -> list[Transactie]:
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM transacties WHERE LOWER(beschrijving) LIKE '%' || LOWER(?) || '%' OR LOWER(van_aan) LIKE '%' || LOWER(?) || '%'", (part_description, part_description))
+        rows = cursor.fetchall()
+        conn.close()
+        transaction_list = [convert_transaction_row_to_object(row) for row in rows]
+        return transaction_list
 
+def save_transaction(transaction: Transactie) -> None:
+    conn = get_connection()
+    cursor = conn.cursor()
 
+    if transaction.id == 0:
+        # Create a new transaction
+        cursor.execute("""
+            INSERT INTO transacties
+            (datum, bedrag, betaalwijze, rekeningnummer, van_aan, beschrijving,
+            factuurnummer, factuurdatum, categorie, actuele_rekeningstand)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+            """, (transaction.datum, transaction.bedrag, transaction.betaalwijze,
+                  transaction.rekeningnummer, transaction.van_aan, transaction.beschrijving,
+                  transaction.factuurnummer, transaction.factuurdatum, transaction.categorie,
+                  transaction.actuele_rekeningstand))
+    else:
+        # Update an existing transaction
+        cursor.execute("""
+            UPDATE transacties
+            SET datum=?, bedrag=?, betaalwijze=?, rekeningnummer=?, van_aan=?, beschrijving=?,
+            factuurnummer=?, factuurdatum=?, categorie=?, actuele_rekeningstand=?
+            WHERE id=?
+            """, (transaction.datum, transaction.bedrag, transaction.betaalwijze,
+                  transaction.rekeningnummer, transaction.van_aan, transaction.beschrijving,
+                  transaction.factuurnummer, transaction.factuurdatum, transaction.categorie,
+                  transaction.actuele_rekeningstand, transaction.id))
+    conn.commit()
+    conn.close()
--- model/transactie.py
+++ model/transactie.py
@@ -2,12 +2,13 @@ from datetime import date
 import logging
 
 class Transactie():
-    def __init__(self, id: int = 0, datum: date = date.today(), 
-                 bedrag: float = 0.0, betaalwijze: str = "", 
-                 rekeningnummer: str = "", van_aan: str = "", 
-                 beschrijving: str = "", factuurnummer: str = "", 
-                 factuurdatum: date = date.today(), categorie: str = "", 
+    def __init__(self, id: int = 0, datum: date = date.today(),
+                 bedrag: float = 0.0, betaalwijze: str = "",
+                 rekeningnummer: str = "", van_aan: str = "",
+                 beschrijving: str = "", factuurnummer: str = "",
+                 factuurdatum: date = date.today(), categorie: str = "",
                  actuele_rekeningstand: float = 0.0):
+        self.id = id
         self.datum = datum
         self.bedrag = bedrag
         self.betaalwijze = betaalwijze
@@ -18,7 +19,6 @@ class Transactie():
         self.factuurdatum = factuurdatum
         self.categorie = categorie
         self.actuele_rekeningstand = actuele_rekeningstand
-        
+
     def __str__(self):
         return f"Datum: {self.datum}. Bedrag: {self.bedrag}. Beschrijving: {self.beschrijving}"
-        
